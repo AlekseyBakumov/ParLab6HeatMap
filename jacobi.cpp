528e3391b149adbb1725f218f032caceb206b719
AlekseyB <a.bakumov@g.nsu.ru>
2025-05-27 02:34:44 +0000

    Added cublas on CalcError

--- jacobi.cpp
+++ jacobi.cpp
@@ -1,10 +1,12 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
 #include <math.h>
 #include <memory>
 #include <omp.h>
 #include <openacc.h>
+#include <cublas_v2.h>
 #include <nvtx3/nvToolsExt.h>
 
 #define OFFSET(x, y, m) (((x)*(m)) + (y))
@@ -64,7 +66,7 @@ double calcNext(double* A, double* Anew, int m, int n)
 {
     double error = 0.0;
     
-    #pragma acc parallel loop collapse(2) present(A, Anew) // reduction(max:error)
+    #pragma acc parallel loop collapse(2) present(A, Anew)  // reduction(max:error)
     for(int j = 1; j < n-1; j++)
     {
         for(int i = 1; i < m-1; i++)
@@ -77,10 +79,10 @@ double calcNext(double* A, double* Anew, int m, int n)
     return error;
 }
 
-double calcError(double* A, double* Anew, int m, int n)
+double calcError(double* A, double* Anew, int m, int n, double* diff)
 {
     double error = 0.0;
-    
+    /*
     #pragma acc parallel loop collapse(2) present(A, Anew) reduction(max:error)
     for(int j = 1; j < n-1; j++)
     {
@@ -89,6 +91,37 @@ double calcError(double* A, double* Anew, int m, int n)
             error = fmax(error, fabs(Anew[OFFSET(j, i, m)] - A[OFFSET(j, i, m)]));
         }
     }
+    /**/
+
+    ///*
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    int size = (n-2)*(m-2);
+
+    #pragma acc data present(A[0:n*m], Anew[0:n*m], diff[0:size])
+    {
+        #pragma acc host_data use_device(A, Anew, diff)
+        {
+            double alpha = 1.0;
+            double beta = -1.0;
+            cublasDgeam(handle, CUBLAS_OP_N, CUBLAS_OP_N,
+                       m-2, n-2, &alpha,
+                       &Anew[OFFSET(1, 1, m)], m,
+                       &beta, &A[OFFSET(1, 1, m)], m,
+                       diff, m-2);
+            
+            int result;
+            cublasIdamax(handle, size, diff, 1, &result);
+            
+            double max_diff;
+            cublasGetVector(1, sizeof(double), &diff[result-1], 1, &max_diff, 1);
+            error = max_diff;
+        }
+    }
+
+    cublasDestroy(handle);
+    /**/
+
     return error;
 }
 
@@ -118,9 +151,11 @@ int main(int argc, char** argv)
 
     auto A_sh = std::shared_ptr<double[]>(new double[n*m]);
     auto Anew_sh = std::shared_ptr<double[]>(new double[n*m]);
+    auto diff_ptr = std::shared_ptr<double[]>(new double[(n-2)*(m-2)]);
 
     double* A = A_sh.get();
     double* Anew = Anew_sh.get();
+    double* diff = diff_ptr.get();
 
     nvtxRangePushA("init");
     initialize(A, Anew, m, n);
@@ -132,7 +167,7 @@ int main(int argc, char** argv)
     int iter = 0;
 
     nvtxRangePushA("ACC Copy");
-    #pragma acc data copy(A[0:n*m], Anew[0:n*m])
+    #pragma acc data copy(A[0:n*m], Anew[0:n*m], diff[0:(n-2)*(m-2)])
     {
         nvtxRangePop();
 
@@ -144,19 +179,20 @@ int main(int argc, char** argv)
             calcNext(A, Anew, m, n);
             nvtxRangePop();
 
+            if(iter % 1000 == 0) 
+            {
+                nvtxRangePushA("error calc");
+                error = calcError(A, Anew, m, n, diff);
+                nvtxRangePop();
+                printf("%5d, %0.6f\n", iter, error);
+            }
+
             nvtxRangePushA("swap");
             //swap(A, Anew, m, n);
             #pragma acc data present(A, Anew)
             std::swap(A, Anew);
             nvtxRangePop();
 
-            if(iter % 1000 == 0) 
-            {
-                nvtxRangePushA("error calc");
-                error = calcError(A, Anew, m, n);
-                nvtxRangePop();
-                printf("%5d, %0.6f\n", iter, error);
-            }
             iter++;
         }
         nvtxRangePop();
@@ -164,6 +200,18 @@ int main(int argc, char** argv)
     
     double runtime = omp_get_wtime() - st;
     printf(" total: %f s\n", runtime);
+
+    printf("Printing matrix in file...\n");
+    std::ofstream out_file;
+    out_file.open("out_matxr.txt");
+
+    out_file << (A[0]);
+    for (int i = 1; i < m*n; i++)
+    {
+        out_file << "," << (A[i]);
+    }
+
+    printf("Result in: out_matxr.txt\n");
 
     return 0;
 }
